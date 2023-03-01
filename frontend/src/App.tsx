48443b6d6604908f28977b48fdaed218fdbfc3d8
Michal Vaniš <zirixcz@gmail.com>
2023-03-01 21:55:07 +0100

    symbol as the background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes, useLocation} from "react-router-dom";
 import Register from "./pages/unprotected/register";
 import Login from "./pages/unprotected/login";
 import styled from "styled-components/macro";
-import Layout from "./pages/@Layout";
 import Leaderboard from "./pages/leaderboard";
 import Lobbies from "./pages/lobbies";
 import Admin from "./pages/admin";
@@ -15,35 +14,48 @@ import Auth from "./pages/Auth";
 import Welcome from "./pages/unprotected/welcome/Welcome";
 
 const App = (): JSX.Element => {
-  return (
-    <Theme>
-      <Body>
-        <Container>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Auth />}>
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="leaderboard" element={<Leaderboard />} />
-                <Route path="lobbies" element={<Lobbies />} />
-                <Route path="lobbies/:name" element={<Game />} />
-                <Route path="theme" element={<ThemeSwitcher />} />
-                <Route path="admin/*" element={<Admin />} />
-              </Route>
-              <Route path="/welcome" element={<Welcome />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-          </BrowserRouter>
-        </Container>
-      </Body>
-    </Theme>
-  );
+    const [symbol, setSymbol] = React.useState<string>();
+    const location = useLocation();
+
+    React.useEffect(() => {
+        // choose from three strings randomly and set it as the symbol
+        const symbols = [
+            "/img/symbolA.svg",
+            "/img/symbolS.svg",
+            "/img/symbolY.svg"
+        ]
+        const symbol = symbols[Math.floor(Math.random() * symbols.length)]
+        setSymbol(symbol)
+    }, [location.pathname])
+
+    return (
+        <Theme>
+            <Body>
+                <Container>
+                    <Symbol src={symbol} alt="Image of a letter symbol"/>
+                    <Routes>
+                        <Route path="/" element={<Auth/>}>
+                            <Route path="dashboard" element={<Dashboard/>}/>
+                            <Route path="leaderboard" element={<Leaderboard/>}/>
+                            <Route path="lobbies" element={<Lobbies/>}/>
+                            <Route path="lobbies/:name" element={<Game/>}/>
+                            <Route path="theme" element={<ThemeSwitcher/>}/>
+                            <Route path="admin/*" element={<Admin/>}/>
+                        </Route>
+                        <Route path="/welcome" element={<Welcome/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="*" element={<Navigate to="/"/>}/>
+                    </Routes>
+                </Container>
+            </Body>
+        </Theme>
+    );
 };
 
 const Body = styled.body`
-  background-color: ${({ theme }) => theme.white};
-  color: ${({ theme }) => theme.text};
+  background-color: ${({theme}) => theme.white};
+  color: ${({theme}) => theme.text};
 `;
 
 export const Container = styled.div`
@@ -51,4 +63,9 @@ export const Container = styled.div`
   width: 100%;
 `;
 
+export const Symbol = styled.img`
+  position: absolute;
+  height: 100%;
+  pointer-events: none;
+`
 export default App;
