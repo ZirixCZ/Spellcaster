4c3c194583b2fe4c2681ec21f33bd2bfa917cda3
Michal Vaniš <zirixcz@gmail.com>
2023-03-20 02:00:24 +0100

    use await to fix logical error #8

--- frontend/src/pages/unprotected/login/index.tsx
+++ frontend/src/pages/unprotected/login/index.tsx
@@ -1,57 +1,80 @@
 import * as React from "react";
-import {FormEvent, useRef, useState} from "react";
+import { FormEvent, useRef, useState } from "react";
 import Button from "../../../components/Button";
-import {Link, useNavigate} from "react-router-dom";
-import styled, {css} from "styled-components/macro";
+import { Link, useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components/macro";
 import FormInput from "../../../components/FormInput";
 import Container from "../../../components/Container";
 import Paragraph from "../../../components/Paragraph";
-import {mobile, tablet} from "../../../Global";
+import { mobile, tablet } from "../../../Global";
 import handleSubmit from "./services/handleSubmit";
 
 const Login = (): JSX.Element => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const userNameRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
 
-    const userNameRef = useRef<HTMLInputElement | null>(null);
-    const passwordRef = useRef<HTMLInputElement | null>(null);
+  const [submitSuccess, setSubmitSuccess] = React.useState<Promise<boolean>>();
 
-    const [submitSuccess, setSubmitSuccess] = React.useState<Promise<boolean>>()
+  React.useEffect(() => {
+    console.log(submitSuccess);
+    if (typeof submitSuccess !== "undefined")
+      submitSuccess.then((result) => {
+        if (!result) return;
+        console.log(result);
+        console.log(localStorage.getItem("jwt"));
 
-    React.useEffect(() => {
-        if (typeof submitSuccess !== "undefined")
-            submitSuccess.then(result => {
-                if (!result) return
+        if (result === true || result === false) navigate("/");
+      });
+  }, [submitSuccess, navigate]);
 
-                if (result === true || result === false)
-                    navigate("/")
-            })
-    }, [submitSuccess])
-
-    return (
-        <Container height={100} justifyContent="center">
-            <StyledContainer paddingTop={0} alignItems="start" paddingTopTablet={2}>
-                <Title>Sign in</Title>
-            </StyledContainer>
-            <Form
-                onSubmit={(e) => setSubmitSuccess(handleSubmit(e, userNameRef?.current?.value, passwordRef?.current?.value))}>
-                <GTitleLeft>ACCOUNT INFORMATION</GTitleLeft>
-                <FormInput refer={userNameRef} placeholder="Username" type="text" pattern="^[a-z0-9_.]+$"
-                           errorMessage="email invalid"/>
-                <FormInput refer={passwordRef} placeholder="Password" type="password"
-                           pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" errorMessage="password invalid"
-                           isLast={true}/>
-                <Paragraph size={1} weight={500}><Link to="/register">I don't have an account</Link></Paragraph>
-                <Button primary medium>Login</Button>
-            </Form>
-        </Container>
-    );
-
-}
+  return (
+    <Container height={100} justifyContent="center">
+      <StyledContainer paddingTop={0} alignItems="start" paddingTopTablet={2}>
+        <Title>Sign in</Title>
+      </StyledContainer>
+      <Form
+        onSubmit={(e) =>
+          setSubmitSuccess(
+            handleSubmit(
+              e,
+              userNameRef?.current?.value,
+              passwordRef?.current?.value
+            )
+          )
+        }
+      >
+        <GTitleLeft>ACCOUNT INFORMATION</GTitleLeft>
+        <FormInput
+          refer={userNameRef}
+          placeholder="Username"
+          type="text"
+          pattern="^[a-z0-9_.]+$"
+          errorMessage="email invalid"
+        />
+        <FormInput
+          refer={passwordRef}
+          placeholder="Password"
+          type="password"
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+          errorMessage="password invalid"
+          isLast={true}
+        />
+        <Paragraph size={1} weight={500}>
+          <Link to="/register">I don't have an account</Link>
+        </Paragraph>
+        <Button primary medium>
+          Login
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
 const Title = styled.h1`
   font-size: 4em;
-`
+`;
 
 const StyledContainer = styled(Container)`
   text-align: start;
@@ -64,14 +87,14 @@ const StyledContainer = styled(Container)`
   ${mobile(css`
     width: 75%;
   `)}
-`
+`;
 
 export const GTitleLeft = styled.p`
   width: 100%;
   font-size: 0.75rem;
   font-weight: 600;
   text-align: left;
-`
+`;
 
 export const Form = styled.form`
   width: 25%;
@@ -88,6 +111,6 @@ export const Form = styled.form`
   ${mobile(css`
     width: 75%;
   `)}
-`
+`;
 
 export default Login;
