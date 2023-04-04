a77ea6858ebb5e5d142098babf043c2d5f0c5c09
Michal Vaniš <zirixcz@gmail.com>
2023-04-04 22:35:27 +0200

    pain pain pain pain why did none of the color picker libraries work???

--- frontend/src/views/Navbar.tsx
+++ frontend/src/views/Navbar.tsx
@@ -9,13 +9,22 @@ interface Props {
 }
 
 const Navbar = (props: Props) => {
+  const navigate = useNavigate();
   const symbol = useSymbolStore((state) => state.symbol);
   const changeSymbol = useSymbolStore((state) => state.changeSymbol);
+  const [symbolState, setSymbolState] = React.useState<string>("");
 
-  const navigate = useNavigate();
+  const parseSymbol = (symbol: string) => {
+    let parse = symbol.split(".")[0];
+    return parse[parse.length - 1];
+  };
+
+  React.useEffect(() => {
+    setSymbolState(parseSymbol(symbol));
+  }, [symbol]);
 
   return (
-    <StyledNavbar>
+    <StyledNavbar symbol={symbolState}>
       <StyledContainer>
         <Item onClick={() => navigate("/")}>Dashboard</Item>
         <Item onClick={() => navigate("/leaderboard")}>Leaderboard</Item>
@@ -27,7 +36,7 @@ const Navbar = (props: Props) => {
 
 const Item = styled.a`
   font-size: 1.25rem;
-  font-weight: 600;
+  font-weight: 500;
   cursor: pointer;
 `;
 
@@ -60,16 +69,130 @@ const StyledContainer = styled.div`
   `)}
 `;
 
-const StyledNavbar = styled.div`
+interface StyledNavbarInterface {
+  symbol: string;
+}
+
+const StyledNavbar = styled.div<StyledNavbarInterface>`
   height: 4rem;
   width: 100%;
   position: absolute;
   z-index: 1;
   background-color: ${({ theme }) => theme.white};
   top: 0;
-  border-bottom-width: 4px;
-  border-bottom-style: solid;
-  border-bottom-image: linear-gradient(to right, darkblue, darkorchid) 1;
+  border-bottom-width: 1.5px;
+  border-top-width: 0;
+  border-style: solid;
+  border-image: ${({ symbol }) =>
+    `linear-gradient(to right, ${
+      symbol === "K"
+        ? "#24da24"
+        : symbol === "A"
+        ? "#22cc11"
+        : symbol === "B"
+        ? "#3fbf3f"
+        : symbol === "C"
+        ? "#00ff2a"
+        : symbol === "D"
+        ? "#ff8c00"
+        : symbol === "E"
+        ? "#ff8c00"
+        : symbol === "F"
+        ? "#1c54c6"
+        : symbol === "G"
+        ? "#2aaaff"
+        : symbol === "H"
+        ? "#00aad4"
+        : symbol === "I"
+        ? "#00b0fe"
+        : symbol === "J"
+        ? "#00b0fe"
+        : symbol === "L"
+        ? "#33e519"
+        : symbol === "M"
+        ? "#df005f"
+        : symbol === "N"
+        ? "#df005f"
+        : symbol === "O"
+        ? "#aa00ff"
+        : symbol === "P"
+        ? "#ff0000"
+        : symbol === "Q"
+        ? "#ff8c00"
+        : symbol === "R"
+        ? "#006ae9"
+        : symbol === "S"
+        ? "#6600cc"
+        : symbol === "T"
+        ? "#00c8ec"
+        : symbol === "U"
+        ? "#0073e7"
+        : symbol === "V"
+        ? "#1c8dc6"
+        : symbol === "W"
+        ? "#00bfff"
+        : symbol === "X"
+        ? "#8d38c6"
+        : symbol === "Y"
+        ? "#9f5fbf"
+        : symbol === "Z"
+        ? "#1966cc"
+        : "black"
+    }, ${
+      symbol === "K"
+        ? "#4600fe"
+        : symbol === "A"
+        ? "#f00e00"
+        : symbol === "B"
+        ? "#000ef0"
+        : symbol === "C"
+        ? "#ff8c00"
+        : symbol === "D"
+        ? "#0045d0"
+        : symbol === "E"
+        ? "#ff8c00"
+        : symbol === "F"
+        ? "#a50f78"
+        : symbol === "G"
+        ? "#d400fe"
+        : symbol === "H"
+        ? "#d22d5a"
+        : symbol === "I"
+        ? "#b42d69"
+        : symbol === "J"
+        ? "#b42d69"
+        : symbol === "L"
+        ? "#3c3cc3"
+        : symbol === "M"
+        ? "#c600f0"
+        : symbol === "N"
+        ? "#c600f0"
+        : symbol === "O"
+        ? "#1f0fff"
+        : symbol === "P"
+        ? "#c600e2"
+        : symbol === "Q"
+        ? "#ff8c00"
+        : symbol === "R"
+        ? "#7faf7f"
+        : symbol === "S"
+        ? "#b4005a"
+        : symbol === "T"
+        ? "#f09b0e"
+        : symbol === "U"
+        ? "#c30fff"
+        : symbol === "V"
+        ? "#8f2fff"
+        : symbol === "W"
+        ? "#00eb75"
+        : symbol === "X"
+        ? "#ef006f"
+        : symbol === "Y"
+        ? "#ff8c00"
+        : symbol === "Z"
+        ? "#71fe00"
+        : "black"
+    }) 1`};
 `;
 
 export default Navbar;
